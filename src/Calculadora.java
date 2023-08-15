1b66cbf3bf4c1e10299858916bc2dc2d1965b749
Daniel Olguin <dani.olg@hotmail.com>
2023-08-14 21:44:29 -0400

    feat: call start functions

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,5 +1,8 @@
+import interfaz.Interfaz;
+
 public class Calculadora {
     public static void main(String[] args) {
-
+        Interfaz interfaz = new Interfaz();
+        interfaz.iniciarInterfaz();
     }
 }
